3e4c8f74b093afac6a8825c8ac726e7c9fcb40fc
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2023-06-21 13:29:21 +0300

    add fetch function

--- script.js
+++ script.js
@@ -1,49 +1,62 @@
-
-const pictures = [
-  "./img/1.jpeg",
-  "./img/2.jpeg",
-  "./img/3.jpeg",
-  "./img/4.jpeg",
-  "./img/5.jpeg",
-  "./img/6.jpeg",
-  "./img/7.jpeg",
-  "./img/8.jpeg",
-  "./img/9.jpeg",
-]
+let pictures = [];
 
 function generateImg() {
-  const container = document.querySelector('.wrap-picture')
-  container.innerHTML = ''
+  const container = document.querySelector('.wrap-picture');
+  container.innerHTML = '';
 
-  let randomIdx = getRandomIndex()
-  let imageSrc = pictures[randomIdx];
+  if (pictures.length > 0) {
+    let randomIdx = getRandomIndex();
+    let imageSrc = pictures[randomIdx];
 
-  const imageElement = document.createElement('img');
-  imageElement.src = imageSrc;
+    const imageElement = document.createElement('img');
+    imageElement.src = imageSrc;
 
-  container.appendChild(imageElement);
-  
-  addPicToCoursel(imageSrc)
+    container.appendChild(imageElement);
+  }
+
+  fetchRandomImage(); // Запрашиваем новую случайную картинку при каждом вызове generateImg()
 }
 
 function getRandomIndex() {
   return Math.floor(Math.random() * pictures.length);
 }
 
-function onGenerateClickHandler (){
-    generateImg()
+function onGenerateClickHandler() {
+  generateImg();
 }
 
 function addPicToCoursel(imageSrc) {
-  const coursel = document.querySelector('.wrap-coursel')
-  const imageInCoursel = document.createElement ('img')
-  imageInCoursel.src = imageSrc
-  coursel.appendChild(imageInCoursel)
+  const coursel = document.querySelector('.wrap-coursel');
+  const imageInCoursel = document.createElement('img');
+  imageInCoursel.src = imageSrc;
+  coursel.appendChild(imageInCoursel);
 }
 
-
-const generateButton = document.querySelector('.generate-btn')
-generateButton.onclick = onGenerateClickHandler 
-
+const generateButton = document.querySelector('.generate-btn');
+generateButton.onclick = onGenerateClickHandler;
 
 
+function fetchRandomImage() {
+  const accessKey = 'e_b8bORlFuCeYhZrun2tsufVV9FcybWl7Kw6yhTBFLM'; // Замените на свой ключ доступа Unsplash API
+  const apiUrl = 'https://api.unsplash.com/photos/random';
+
+  axios.get(apiUrl, {
+      headers: {
+        'Authorization': 'Client-ID ' + accessKey
+      }
+    })
+    .then(response => {
+      const imageSrc = response.data.urls.regular;
+      pictures.push(imageSrc);
+      addPicToCoursel(imageSrc);
+    })
+    .catch(error => {
+      console.error('Ошибка при запросе случайного изображения:', error);
+    });
+}
+
+function displayImage(imageSrc) {
+  console.log(imageSrc);
+}
+
+fetchRandomImage();
