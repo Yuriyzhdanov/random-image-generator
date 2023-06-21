82e28b68a3be47a2205d6c2c4541c4d099b5fe16
Yuriyzhdanov <yuuriyzhdanov@gmail.com>
2023-06-21 18:15:11 +0300

    realize fnuction and fix bags

--- script.js
+++ script.js
@@ -1,62 +1,74 @@
-let pictures = [];
+const accessKey = "e_b8bORlFuCeYhZrun2tsufVV9FcybWl7Kw6yhTBFLM"; // Замените на свой ключ доступа Unsplash API
+const apiUrl = "https://api.unsplash.com/photos/random";
 
-function generateImg() {
-  const container = document.querySelector('.wrap-picture');
-  container.innerHTML = '';
+// Константы для доступа к элементам DOM
+const imageContainer = document.getElementById("imageContainer");
+const courselWrap = document.querySelector(".wrap-coursel");
+const headWrap = document.querySelector(".wrap-head");
 
-  if (pictures.length > 0) {
-    let randomIdx = getRandomIndex();
-    let imageSrc = pictures[randomIdx];
+// Массив для хранения полученных изображений
+const pictures = [];
 
-    const imageElement = document.createElement('img');
-    imageElement.src = imageSrc;
+// Функция для асинхронного получения и сохранения изображения
+async function fetchAndSaveImage() {
+  try {
+    const response = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Client-ID ${accessKey}`,
+      },
+    });
 
-    container.appendChild(imageElement);
+    const imageData = response.data;
+    pictures.push(imageData);
+
+    return imageData;
+  } catch (error) {
+    console.error("Ошибка при получении изображения:", error);
+    throw error;
+  }
+}
+
+// Функция для отображения изображения с классом "wrap-picture"
+function displayImage() {
+  const lastImageIndex = pictures.length - 1;
+  const image = pictures[lastImageIndex];
+
+  if (imageContainer) {
+    imageContainer.innerHTML = `<img src="${image.urls.regular}" class="wrap-picture" alt="${image.alt_description}">`;
+  }
+}
+
+// Функция для сохранения текущего изображения с классом "wrap-coursel"
+function saveAndDisplayCurrentImage() {
+  const currentImageIndex = pictures.length - 1;
+  const currentImage = pictures[currentImageIndex];
+
+  if (courselWrap) {
+    const newImageElement = document.createElement("img");
+    newImageElement.src = currentImage.urls.regular;
+    newImageElement.className = "wrap-coursel";
+    courselWrap.appendChild(newImageElement);
   }
 
-  fetchRandomImage(); // Запрашиваем новую случайную картинку при каждом вызове generateImg()
+  if (headWrap) {
+    headWrap.innerHTML = `<h4>${currentImage.alt_description} by ${currentImage.user.name}</h4>`;
+  }
 }
 
-function getRandomIndex() {
-  return Math.floor(Math.random() * pictures.length);
+// Обработчик события для кнопки "Generate"
+const generateBtn = document.querySelector(".generate-btn");
+if (generateBtn) {
+  generateBtn.addEventListener("click", () => {
+    fetchAndSaveImage()
+      .then(() => {
+        // Вызываем функцию displayImage для отображения изображения
+        displayImage();
+
+        // Вызываем функцию saveAndDisplayCurrentImage для сохранения и отображения данных изображения
+        saveAndDisplayCurrentImage();
+      })
+      .catch((error) => {
+        console.error("Ошибка:", error);
+      });
+  });
 }
-
-function onGenerateClickHandler() {
-  generateImg();
-}
-
-function addPicToCoursel(imageSrc) {
-  const coursel = document.querySelector('.wrap-coursel');
-  const imageInCoursel = document.createElement('img');
-  imageInCoursel.src = imageSrc;
-  coursel.appendChild(imageInCoursel);
-}
-
-const generateButton = document.querySelector('.generate-btn');
-generateButton.onclick = onGenerateClickHandler;
-
-
-function fetchRandomImage() {
-  const accessKey = 'e_b8bORlFuCeYhZrun2tsufVV9FcybWl7Kw6yhTBFLM'; // Замените на свой ключ доступа Unsplash API
-  const apiUrl = 'https://api.unsplash.com/photos/random';
-
-  axios.get(apiUrl, {
-      headers: {
-        'Authorization': 'Client-ID ' + accessKey
-      }
-    })
-    .then(response => {
-      const imageSrc = response.data.urls.regular;
-      pictures.push(imageSrc);
-      addPicToCoursel(imageSrc);
-    })
-    .catch(error => {
-      console.error('Ошибка при запросе случайного изображения:', error);
-    });
-}
-
-function displayImage(imageSrc) {
-  console.log(imageSrc);
-}
-
-fetchRandomImage();
